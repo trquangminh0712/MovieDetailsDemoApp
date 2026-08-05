0f73a44300ef586a0735c9406ea97a06224abbaa
Tran Quang Minh <trquangminh0712@gmail.com>
2026-08-05 20:26:57 +0700

    1st commit

---
+++ app/src/main/java/com/quangminh/moviedetailsdempapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.quangminh.moviedetailsdempapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
